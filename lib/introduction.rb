72aa1cce9df9cbc8a4f8854a23baf821627352a4
Tanesha Goodman <t24good@icloud.com>
2019-10-17 21:16:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,9 +11,11 @@ end
 
 name = "Dan", language = "Ember"
 
+def introduction_with_language_optional(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
-
-
+name = "Edwin", language= "Ruby"
 
 
 
