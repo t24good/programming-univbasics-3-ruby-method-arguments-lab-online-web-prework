b9dedcd179c3976fc5dc890d6b3f56bf51f3da66
Tanesha Goodman <t24good@icloud.com>
2019-10-17 21:15:09 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,7 @@ name = "Josh"
 
 
 def introduction_with_language(name, language)
-  puts "Hi my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 name = "Dan", language = "Ember"
